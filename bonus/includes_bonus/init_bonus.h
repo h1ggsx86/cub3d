0419d2b45becab551bbfadf65c15f8ce0f027cd6
k3r0z3ne <andreartd0@gmail.com>
2025-04-08 11:40:43 +0200

    splitting bonus

---
+++ bonus/includes_bonus/init_bonus.h
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_bonus.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arotondo <arotondo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/25 18:33:13 by arotondo          #+#    #+#             */
+/*   Updated: 2025/04/08 11:19:12 by arotondo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INIT_BONUS_H
+# define INIT_BONUS_H
+
+typedef struct s_ivector	t_ivector;
+typedef struct s_fvector	t_fvector;
+typedef struct s_ray		t_ray;
+typedef struct s_color		t_color;
+typedef struct s_mimg		t_mimg;
+typedef struct s_data		t_data;
+typedef struct s_player		t_player;
+typedef struct s_game		t_game;
+
+/* init_game_bonus */
+int		img_init(t_mimg *img, t_game *g, void *init);
+void	game_init(t_game *g);
+void	init_imgs(t_game *g, t_data *d, int size);
+void	init_ray(t_ray *r);
+
+/* others_init_bonus */
+void	ivector_init(t_ivector *vec, int x, int y);
+void	fvector_init(t_fvector *vec, double x, double y);
+void	player_init(t_player *pl);
+void	key_init(t_game *new);
+void	init_doors(t_game *g, int *size);
+
+/* init_struct_bonus */
+int		init_door_texture(t_data *data);
+int		data_init(t_data *data);
+void	data_init2(t_data *data);
+int		color_init(t_data *data);
+void	struct_init(t_game *new, t_data *data, t_player *pl);
+
+/* get_textures_bonus */
+t_mimg	*init_north_texture(t_game *g);
+t_mimg	*init_south_texture(t_game *g);
+t_mimg	*init_west_texture(t_game *g);
+t_mimg	*init_east_texture(t_game *g);
+
+#endif
---
+++ bonus/includes_bonus/utils_bonus.h
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_bonus.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arotondo <arotondo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/25 18:26:43 by arotondo          #+#    #+#             */
+/*   Updated: 2025/04/08 11:16:45 by arotondo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef UTILS_BONUS_H
+# define UTILS_BONUS_H
+
+typedef struct s_ivector	t_ivector;
+typedef struct s_fvector	t_fvector;
+typedef struct s_color		t_color;
+typedef struct s_mimg		t_mimg;
+typedef struct s_data		t_data;
+typedef struct s_player		t_player;
+typedef struct s_game		t_game;
+
+/* exit_game */
+void	err_message(t_game *g, char *arg, char *mess, int ecode);
+void	exit_game(t_game *g, int ecode);
+
+/* put_pixel */
+int		pixel_color(t_mimg texture, int x, int y);
+void	put_pixel(t_data *d, int x, int y, int color);
+void	put_square(t_game *g, int xc, int yc, int c);
+void	put_player_line(t_game *g, t_ivector p, int x, int y);
+
+/* put_pixel_lines */
+void	put_line_l(t_game *g, t_ivector start, int x1, int y1);
+void	put_line_h(t_game *g, t_ivector start, int x1, int y1);
+
+/* moves input */
+int		mouse_move(t_game *g);
+void	moves_input(t_game *g);
+
+/* ft_get_time */
+long	ft_get_time(void);
+
+/* get trgb*/
+int		get_t(int trgb);
+int		get_r(int trgb);
+int		get_g(int trgb);
+int		get_b(int trgb);
+int		apply_intensity(int color, double intensity);
+
+/* free */
+void	free_img(void *init, t_mimg *img);
+void	free_doors(void *init, t_game *g);
+void	free_incomp_text(t_game *g);
+void	free_textures(void *init, t_mimg *img, t_game *g);
+void	free_map(char **map);
+
+#endif
